e7e39c4f730b1403894a957aa62e36098360f18c
thuyltm <thuyltm2201@gmail.com>
2016-12-09 07:11:26 +0700

    Fix Freq_Finish Time

--- Preprocess/src/bk/master/input/FileUtil.java
+++ Preprocess/src/bk/master/input/FileUtil.java
@@ -1,15 +1,50 @@
 package bk.master.input;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.FilenameUtils;
 
+import bk.master.input.model.Route;
+
 public class FileUtil {
+    public static void addDepartDestFileName(String dataPath, String changeFolder, String day) {
+        List<Route> routeList = InputUtil.loadFinishTime(dataPath);
+        File file = new File(dataPath);
+        String deviceId = FilenameUtils.getBaseName(file.getName());
+        for (Route route : routeList) {
+            int index = route.getIndex() - 2;
+            String depart = route.getDeparture();
+            if ("benXeAnSuong".equalsIgnoreCase(depart)) {
+                depart = "AS";
+            }
+            if ("benXeCuChi".equalsIgnoreCase(depart)) {
+                depart = "CC";
+            }
+            String destination = route.getDestination();
+            if ("benXeAnSuong".equalsIgnoreCase(destination)) {
+                destination = "AS";
+            }
+            if ("benXeCuChi".equalsIgnoreCase(destination)) {
+                destination = "CC";
+            }
+            String duration = route.getFinishTimeText();
+            String oldName = "distance_"+deviceId+"_"+day+"_"+ index +"_" +duration;
+            String newName = oldName + "_"+depart+"-"+destination;
+            Path source = Paths.get(changeFolder+oldName+".csv");
+            try {
+                Files.move(source, source.resolveSibling(newName+".csv"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public static void renameFile(String folderPath, String oldNumbber, String newNumber) {
         try {
             File folder = new File(folderPath);
--- Preprocess/src/bk/master/input/Test.java
+++ Preprocess/src/bk/master/input/Test.java
@@ -1,16 +1,50 @@
 package bk.master.input;
 
-import java.util.HashMap;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FilenameUtils;
 
 
 
 public class Test {
     public static void main(String[] args) {
-        HashMap<Integer, List<String>> result =
-                InputUtil.mapFinishTime("/home/thuy1/git/predictUsingProbability/Preprocess/AS_CC.txt");
-        ExportUtil.exportFrequenceFinishTime(result, "AS_CC_Freq.txt");
-        result = InputUtil.mapFinishTime("/home/thuy1/git/predictUsingProbability/Preprocess/CC_AS.txt");
-        ExportUtil.exportFrequenceFinishTime(result, "CC_AS_Freq.txt");
+        File folder = new File("/home/thuy1/git/predictUsingProbability/MongoOperator/09/location/18");
+        File[] dataFile = folder.listFiles();
+        for (File file : dataFile) {
+            String fileName = FilenameUtils.getBaseName(file.getAbsolutePath());
+            String remainName = fileName.substring(0, fileName.lastIndexOf("_")+1);
+            String newName = remainName+"CC-AS";
+            if (fileName.contains("An Suong-Cu Chi")) {
+                newName = remainName+"AS-CC";
+            }
+            //System.out.println(newName);
+            Path source = Paths.get(file.getAbsolutePath());
+            try {
+                Files.move(source, source.resolveSibling(newName+".csv"));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+
+        /*File dataFolder = new File("/home/thuy1/git/predictUsingProbability/GSOperator/newLeg/09/02/");
+        File[] dataList = dataFolder.listFiles();
+        for (File file : dataList) {
+            Path source = Paths.get(file.getAbsolutePath());
+            String fileName = FilenameUtils.getBaseName(file.getName());
+            String newFileName = fileName.substring(0, fileName.lastIndexOf("."));
+            try {
+                Files.move(source, source.resolveSibling(newFileName+".csv"));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }*/
+
     }
 }
