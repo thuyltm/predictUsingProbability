3de354f6d44ea4d208cce755a0cde28c23a668cc
thuyltm <thuyltm2201@gmail.com>
2017-03-06 09:25:12 +0700

    Add more method to check

--- Preprocess/src/bk/master/classify/Check.java
+++ Preprocess/src/bk/master/classify/Check.java
@@ -2,10 +2,15 @@ package bk.master.classify;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ThreadLocalRandom;
 
 import bk.master.util.ExportUtil;
 
@@ -31,8 +36,42 @@ public class Check {
             e.printStackTrace();
         }
     }
+    public static void shuffle(String inputFile, String outputFile) {
+        List<String> data = new ArrayList<String>();
+        List<String> suffleData = new ArrayList<String>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            String nextLine;
+            while ((nextLine = reader.readLine()) != null) {
+                data.add(nextLine);
+            }
+            reader.close();
+            Set<Integer> randomIndexList = generateRandomNotRepeat(190);
+            for (Integer randomIndex : randomIndexList) {
+                suffleData.add(data.get(randomIndex));
+            }
+            ExportUtil.exportToFile(suffleData, outputFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static Set<Integer> generateRandomNotRepeat(int size) {
+        // Note: use LinkedHashSet to maintain insertion order
+        Set<Integer> randomNumberList = new LinkedHashSet<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        for(int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        while(list.size() > 1) {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, list.size()-1);
+            randomNumberList.add(list.get(randomNum));
+            list.remove(randomNum);
+        }
+        randomNumberList.add(list.get(0));
+        list.remove(0);
+        return randomNumberList;
+    }
     public static void main(String[] args) {
-        getReplicate("classifyRoute.csv", "classifyRouteRep_Org.csv");
-        getReplicate("classifyRouteV2.csv", "classifyRouteRep.csv");
+        shuffle("classifyRouteShort.csv", "classifyRouteSuffle.csv");
     }
 }
