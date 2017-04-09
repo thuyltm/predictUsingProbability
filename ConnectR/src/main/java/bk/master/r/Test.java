58bd43bc9864189a8782fe0c8c0a5b4f6241a1ba
thuyltm <thuyltm2201@gmail.com>
2017-04-09 20:26:50 +0700

    connect R with Java

--- ConnectR/src/main/java/bk/master/r/Test.java
+++ ConnectR/src/main/java/bk/master/r/Test.java
@@ -1,14 +1,16 @@
 package bk.master.r;
 
-import java.util.Map;
-
 import org.rosuda.JRI.Rengine;
 
 public class Test {
+    static {
+       // System.out.println(System.getProperty("java.library.path"));
+        //System.loadLibrary("libjri.so");
+    }
     public static void main(String[] args) {
         // Create an R vector in the form of a string.
         String javaVector = "c(141,192,341,177,143,41,99,137,210,307,320,76,34,3,240,2106,1550,262,399,226,313,29,184,323,2421,351,2350,2353,195,409,441,2559,334,212,377,216,405,423,475,539,466,403,410,338,107,279,309,182,428,469,451,59,163,347,206,401,193,348,393,132,384,480,425,1358,2107,137,252,52,313,354,121,328,448,209,1952,1942,1200)";
-       // System.out.println(System.getProperty("java.library.path"));
+        System.out.println(System.getProperty("java.library.path"));
         // Start Rengine.
         Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
 
